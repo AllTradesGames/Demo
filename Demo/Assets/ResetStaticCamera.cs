3c41f2c59a6576add1965d6ec38c6da3d80b4620
AllTradesGames <alltradesgames@gmail.com>
2016-08-05 16:01:54 -0600

    Camera RenderTexture Implementation [2.9]
    Most details in "ResetStaticCamera.cs"

---
+++ Demo/Assets/ResetStaticCamera.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResetStaticCamera : MonoBehaviour
+{
+    public float timeDelay;
+    public RenderTexture gameTexture;
+
+    private Vector3 origPos;
+    private Quaternion origRot;
+    private Vector3 origScale;
+    private Transform backgroundPlane;
+
+    private bool set = false;
+
+    void Start()
+    {
+        backgroundPlane = Camera.main.transform.FindChild("BackgroundPlane").transform;
+
+
+    }
+
+    void Update()
+    {
+        if (!set && (Time.time > timeDelay))
+        {
+            GetComponent<Camera>().projectionMatrix = Camera.main.projectionMatrix;
+
+            origPos = backgroundPlane.localPosition;
+            origRot = backgroundPlane.localRotation;
+            origScale = backgroundPlane.localScale;
+            backgroundPlane.parent = transform;
+
+            backgroundPlane.localPosition = origPos;
+            backgroundPlane.localRotation = origRot;
+            backgroundPlane.localScale = origScale;
+
+            Camera.main.targetTexture = gameTexture;
+
+            set = true;
+        }
+    }
+
+	
+}
