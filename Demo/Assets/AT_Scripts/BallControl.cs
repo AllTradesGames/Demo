964e6ae5e5f34eca44ae403fd772cfdc6d9383be
AllTradesGames <alltradesgames@gmail.com>
2016-08-08 17:54:46 -0600

    Create Ball [0.3]

---
+++ Demo/Assets/AT_Scripts/BallControl.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallControl : MonoBehaviour
+{
+
+	// Use this for initialization
+	void Start()
+    {
+	
+	}
+	
+
+	// Update is called once per frame
+	void Update()
+    {
+	
+	}
+
+
+    void OnCollisionEnter2D(Collision2D coll)
+    {
+        switch(coll.gameObject.tag)
+        {
+            case "test":
+                Debug.Log("Test");
+                break;
+
+        }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        switch (other.tag)
+        {
+            case "test":
+                Debug.Log("Test");
+                break;
+
+        }
+    }
+
+
+}
