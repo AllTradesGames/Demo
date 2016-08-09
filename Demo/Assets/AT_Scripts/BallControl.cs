f3793005fadbeace2810c9a122cbec5378411a85
AllTradesGames <alltradesgames@gmail.com>
2016-08-08 18:41:19 -0600

    Ball Launcher [0.8]

--- Demo/Assets/AT_Scripts/BallControl.cs
+++ Demo/Assets/AT_Scripts/BallControl.cs
@@ -3,11 +3,12 @@ using System.Collections;
 
 public class BallControl : MonoBehaviour
 {
+    public Vector3 startPosition;
 
 	// Use this for initialization
 	void Start()
     {
-	
+        transform.localPosition = startPosition;
 	}
 	
 
@@ -22,8 +23,9 @@ public class BallControl : MonoBehaviour
     {
         switch(coll.gameObject.tag)
         {
-            case "test":
-                Debug.Log("Test");
+            case "Reset":
+                LauncherControl.launchable = true;
+                Debug.Log("Reset");
                 break;
 
         }
---
+++ Demo/Assets/AT_Scripts/LauncherControl.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LauncherControl : MonoBehaviour
+{
+    public static bool launchable = true;
+
+    public float forcePerSec;
+    public float forceCap;
+    public Rigidbody2D ball;
+
+    private float currentForce = 0f;
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Input.GetButton("Submit") && (currentForce < forceCap))
+        {
+            // Increase launch force while charging
+            currentForce += forcePerSec * Time.deltaTime;
+            //Debug.Log(currentForce);
+        }
+        else if(Input.GetButtonUp("Submit"))
+        {
+            // Apply launch force to Ball
+            if (launchable)
+            {
+                ball.AddForce(Vector2.up * currentForce);
+                launchable = false;
+            }
+
+            currentForce = 0f;
+        }
+	}
+
+
+
+
+
+}
